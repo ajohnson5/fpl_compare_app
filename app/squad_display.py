b05e1633a40e433ff2c666f8a30dfd072b9c854e
Alfie Johnson <ag.johnson1@hotmail.com>
2023-06-26 13:56:19 +0100

    Add variable for tailwind style to reduce repetition and convert long strings to multiline

--- app/squad_display.py
+++ app/squad_display.py
@@ -13,10 +13,49 @@ from fpl_api_getters import manager_gw_picks_api
 from player import Player
 from squad import Squad
 
-
+card_common_style = (
+    "col-span-1 row-span-1 flex justify-center content-center "
+    "items-center max-h-[20px]"
+)
 card_width = " w-[60px] xs:w-[70px] sm:w-[85px] md:w-[100px] lg:w-[115px] "
 card_height = " h-full xs:h-[70px] sm:h-[85px] md:h-[100px] lg:h-[115px] "
 shirt_width = " w-[30px] xs:w-[40px] sm:w-[50px] md:w-[60px] lg:w-[70px] "
+
+shirt_image_div = (
+    "col-span-1 row-span-2 grid-cols-1 grid-rows-1 flex "
+    "justify-center items-center relative"
+)
+
+
+player_name_label = (
+    "text-white text-center align-middle text-xs md:texts-sm "
+    "font-medium overflow-hidden truncate leading-tight "
+    "tracking-tighter line-clamp-1"
+)
+
+player_points_div = (
+    "col-span-1 row-span-1 w-full max-h-[20px] flex justify-center "
+    "content-center items-center bg-slate-400/60"
+)
+
+player_points_label = (
+    "text-white text-center align-top text-xs md:texts-sm "
+    "font-medium truncate overflow-hidden leading-tight "
+    "tracking-tighter"
+)
+
+
+bench_card_generator_div = (
+    "flex flex-row lg:flex-col flex-1 h-full justify-around "
+    "content-center items-center gap-x-0"
+)
+
+
+full_bench_div = (
+    "grid grid-cols-1 col-span-5 h-[210px] min-h-[190px] sm:h-[250px] "
+    "md:h-[300px] lg:h-full lg:col-span-1 border-double border-4 "
+    "border-slate-200 rounded-md lg:ml-4 divide-y-4 divide-double"
+)
 
 
 def standard_player_card(player, home: bool):
@@ -26,9 +65,7 @@ def standard_player_card(player, home: bool):
         with ui.element("div").classes(
             "grid grid-cols-1 grid-rows-3  h-full" + card_width
         ):
-            with ui.element("div").classes(
-                "col-span-1 row-span-2 grid-cols-1 grid-rows-1  flex justify-center items-center relative"
-            ):
+            with ui.element("div").classes(shirt_image_div):
                 ui.image("https://i.ibb.co/zsQThP3/ARS-2223-HK-PL-S1.webp").classes(
                     "cols-span-1 row-span-1 object-contain" + shirt_width
                 )
@@ -53,32 +90,22 @@ def standard_player_card(player, home: bool):
                     card_color = "bg-red-500"
 
                 with ui.element("div").classes(
-                    "col-span-1 row-span-1 flex justify-center content-center items-center max-h-[20px]"
-                    + card_width
-                    + card_color
+                    card_common_style + card_width + card_color
                 ):
-                    ui.label(player.name).classes(
-                        "text-white text-center align-middle text-xs md:texts-sm font-medium overflow-hidden truncate leading-tight tracking-tighter line-clamp-1"
-                    )
+                    ui.label(player.name).classes(player_name_label)
 
-                with ui.element("div").classes(
-                    "col-span-1 row-span-1 w-full max-h-[20px] flex justify-center content-center items-center bg-slate-400/60"
-                ):
-                    ui.label(player.actual_points).classes(
-                        "text-white text-center align-top text-xs md:texts-sm font-medium truncate overflow-hidden leading-tight tracking-tighter "
-                    )
+                with ui.element("div").classes(player_points_div):
+                    ui.label(player.actual_points).classes(player_points_label)
 
 
 def standard_bench_card(player, home: bool):
     with ui.element("div").classes(
-        "flex flex-grow flex-1 h-full lg:w-full  items-center justify-center content-center"
+        "flex flex-1 h-full lg:w-full items-center justify-center content-center"
     ):
         with ui.element("div").classes(
             "grid grid-cols-1 grid-rows-3  h-full" + card_width
         ):
-            with ui.element("div").classes(
-                "col-span-1 row-span-2 grid-cols-1 grid-rows-1  flex justify-center items-center relative"
-            ):
+            with ui.element("div").classes(shirt_image_div):
                 ui.image("https://i.ibb.co/zsQThP3/ARS-2223-HK-PL-S1.webp").classes(
                     "cols-span-1 row-span-1 object-contain" + shirt_width
                 )
@@ -86,7 +113,10 @@ def standard_bench_card(player, home: bool):
                     ui.icon("copyright").classes("w-1/5 h-1/5 absolute top-1 right-1")
 
             with ui.element("div").classes(
-                "col-span-1 row-span-1 grid grid-col-1 grid-rows-2 w-full max-h-[40px] pb-2 lg:pb-0"
+                (
+                    "col-span-1 row-span-1 grid grid-col-1 grid-rows-2 w-full "
+                    "max-h-[40px] pb-2 lg:pb-0"
+                )
             ):
                 if home:
                     card_color = "bg-blue-500"
@@ -94,42 +124,29 @@ def standard_bench_card(player, home: bool):
                     card_color = "bg-red-500"
 
                 with ui.element("div").classes(
-                    "col-span-1 row-span-1 flex justify-center content-center items-center max-h-[20px]"
-                    + card_width
-                    + card_color
+                    card_common_style + card_width + card_color
                 ):
-                    ui.label(player.name).classes(
-                        "text-white text-center align-middle text-xs md:texts-sm font-medium overflow-hidden truncate leading-tight tracking-tighter line-clamp-1"
-                    )
+                    ui.label(player.name).classes(player_name_label)
 
-                with ui.element("div").classes(
-                    "col-span-1 row-span-1 w-full max-h-[20px] flex justify-center content-center items-center bg-slate-400/60"
-                ):
-                    ui.label(player.points).classes(
-                        "text-white text-center align-top text-xs md:texts-sm font-medium truncate overflow-hidden leading-tight tracking-tighter "
-                    )
+                with ui.element("div").classes(player_points_div):
+                    ui.label(player.points).classes(player_points_label)
 
 
 def row_generator(player_list: List[Player], home: bool):
     with ui.row().classes(
         "flex flex-row  w-full h-1/4 justify-around content-center gap-x-0"
     ):
-        # with ui.row().classes('w-full h-1/4 grid-cols-5 justify-around w-full gap-x-0.5 content-center') as tester:
         for player in player_list:
             standard_player_card(player, home)
 
 
 def row_generator_bench(player_list: List[Player], home: bool, chip):
     if (chip is not None) and (chip == "bboost"):
-        with ui.row().classes(
-            "flex flex-row lg:flex-col flex-1 h-full justify-around content-center items-center gap-x-0 bg-green-100"
-        ):
+        with ui.row().classes(bench_card_generator_div + "bg-green-100"):
             for player in player_list:
                 standard_bench_card(player, home)
     else:
-        with ui.row().classes(
-            "flex flex-row lg:flex-col flex-1 h-full justify-around content-center items-center gap-x-0"
-        ):
+        with ui.row().classes(bench_card_generator_div):
             for player in player_list:
                 standard_bench_card(player, home)
 
@@ -185,23 +202,13 @@ def show_squad(
                             row_generator(team_2[2], False)
                             row_generator(team_2[1], False)
 
-            with ui.element("div").classes(
-                "grid grid-cols-1 col-span-5 h-[210px] min-h-[190px] sm:h-[250px] md:h-[300px] lg:h-full lg:col-span-1 border-double border-4 border-slate-200 rounded-md  lg:ml-4 divide-y-4 divide-double"
-            ):
+            with ui.element("div").classes(full_bench_div):
                 with ui.element("div").classes(
-                    "col-span-1 min-h-[100px] h-full flex flex-col grow "
+                    "col-span-1 min-h-[100px] h-full flex flex-col grow"
                 ):
                     row_generator_bench(team_1[0], True, squad_1.chip)
 
                 with ui.element("div").classes("col-span-1 min-h-[100px]"):
                     row_generator_bench(team_2[0], False, squad_2.chip)
-
-            # with ui.element('div').classes('flex flex-col flex-1 w-full lg:w-1/5 bg-green-500') as bench_container:
-            #     with ui.element('div').classes('flex-1 w-full  border-double border-4 border-slate-200 rounded-md divide-y-2 divide-double') as bench:
-            #         with ui.element('div').classes('w-full h-1/2 bg-purple-200 ') as bench_home:
-            #             row_generator_bench(team_1[0],True)
-
-            #         with ui.element('div').classes('w-full h-1/2 min-h-[100px]') as bench_away:
-            #             row_generator_bench(team_2[0],False)
 
     return complete_div
