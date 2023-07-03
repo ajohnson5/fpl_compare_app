a2c8cd8ee90dbbe9342afb5b154df1e8c51f3951
Alfie Johnson <ag.johnson1@hotmail.com>
2023-07-03 10:08:31 +0100

    Add tabs with different search bars
    Rather than having a single page web-app I can utilise tabs and have a tab for each different search bar - This means the display squad is not lost when navigating between search bars

--- app/manager_id.py
+++ app/manager_id.py
@@ -9,8 +9,80 @@ import asyncio
 
 
 from squad_display import show_squad
-from search import manager_id_search_bar_execute
+from search import (
+    manager_id_search,
+    top_search,
+    search,
+    manager_id_search_bar,
+    top_50_search,
+    mini_league_search_bar,
+)
+
+manager_id_1, manager_id_2, gameweek, search_button = manager_id_search_bar()
 
 
 def show_page():
-    manager_id_search_bar_execute()
+    with ui.element("div").classes(
+        "flex flex-row justify-center items-center content-center"
+    ):
+        with ui.element("div").classes("w-full max-w-[500px] px-2"):
+            with ui.tabs() as tabs:
+                manager_id_search_tab = ui.tab("Manager ID")
+                mini_league_search_tab = ui.tab("Mini League")
+                top_5_search_tab = ui.tab("Top 5")
+            ui.separator()
+            with ui.tab_panels(
+                tabs, value=manager_id_search_tab, animated=False
+            ).classes("w-full"):
+                with ui.tab_panel(manager_id_search_tab):
+                    (
+                        manager_id_1_tab_1,
+                        manager_id_2_tab_1,
+                        gameweek_tab_1,
+                        search_button_tab_1,
+                    ) = manager_id_search_bar()
+                with ui.tab_panel(mini_league_search_tab):
+                    (
+                        manager_id_1_tab_2,
+                        manager_id_2_tab_2,
+                        gameweek_tab_2,
+                        search_button_tab_2,
+                    ) = mini_league_search_bar()
+                with ui.tab_panel(top_5_search_tab):
+                    (
+                        manager_id_1_tab_3,
+                        manager_id_2_tab_3,
+                        gameweek_tab_3,
+                        search_button_tab_3,
+                    ) = top_50_search()
+
+    error_message = ui.element("div")
+
+    complete_div = ui.element("div").classes("w-full h-auto")
+
+    search(
+        manager_id_1_tab_1,
+        manager_id_2_tab_1,
+        gameweek_tab_1,
+        search_button_tab_1,
+        error_message,
+        complete_div,
+    )
+
+    search(
+        manager_id_1_tab_2,
+        manager_id_2_tab_2,
+        gameweek_tab_2,
+        search_button_tab_2,
+        error_message,
+        complete_div,
+    )
+
+    search(
+        manager_id_1_tab_3,
+        manager_id_2_tab_3,
+        gameweek_tab_3,
+        search_button_tab_3,
+        error_message,
+        complete_div,
+    )
