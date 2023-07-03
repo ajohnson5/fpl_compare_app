207729fe2fd7fb3bf884b896c52e95fa6b5c4059
Alfie Johnson <ag.johnson1@hotmail.com>
2023-07-03 11:09:45 +0100

    Remove individual pages for search and implement single compare page

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from common_format import display
-import page_creation
+import compare
+import index
 import asyncio
 from nicegui import ui, Client, globals
 from fastapi import FastAPI
@@ -20,27 +21,17 @@ def main(client: Client):
     router = Router()
 
     @router.add("/")
-    def home():
-        page_creation.home_page()
+    def home_page():
+        index.show_page()
 
-    @router.add("/search_manager_id")
-    def manager_id():
-        page_creation.by_manager_id_page()
-
-    @router.add("/search_mini_league")
-    def mini_league():
-        page_creation.by_mini_league_page()
-
-    @router.add("/search_top")
-    def top_managers():
-        page_creation.by_top_5_page()
+    @router.add("/compare")
+    def compare_page():
+        compare.show_page()
 
     display(
         [
-            (home, "Home"),
-            (manager_id, "Manager ID"),
-            (mini_league, "Mini League"),
-            (top_managers, "Top 5"),
+            (home_page, "Home"),
+            (compare_page, "Compare"),
         ],
         router,
     )
