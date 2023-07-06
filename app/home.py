cb56f4bb171dc9402ceb248df7c5eb7e3eade65e
Alfie Johnson <ag.johnson1@hotmail.com>
2023-07-06 21:56:42 +0100

    Add add/delete chip basic functionality

--- app/home.py
+++ app/home.py
@@ -19,6 +19,31 @@ from search import (
 )
 
 
+def manager_chip(manager_name: str, home: bool):
+    if home:
+        chip_bg = " bg-blue-500"
+    else:
+        chip_bg = " bg-red-500"
+
+    with ui.element("div").classes(
+        "w-[210px] h-[40px] rounded-lg outline outline-offset-4 "
+        "outline-white" + chip_bg
+    ) as chip:
+        with ui.row().classes(
+            "w-full h-full flex flex-row justify-between content-center " "items-center"
+        ):
+            manager_name = ui.label(manager_name).classes(
+                "text-white pl-2 line-clamp-1 max-w-[150px]"
+            )
+            delete_chip = ui.icon("cancel", size="25px").classes(
+                "cursor-pointer pr-2 text-slate-50 hover:text-slate-400"
+            )
+
+    chip.style("visibility:hidden")
+
+    return chip, manager_name, delete_chip
+
+
 async def show_page():
     with ui.element("div").classes(
         (
@@ -36,43 +61,54 @@ async def show_page():
         input_1.classes("w-2/3 max-w-[300px]")
         gw_select_1.classes("w-1/3 max-w-[100px]")
 
+        chip_state = {"chip_1": None, "chip_2": None}
+
+        def add_chip(name):
+            if name:
+                if not chip_state["chip_1"]:
+                    chip_state["chip_1"] = name
+                    chip_1.style("visibility:visible")
+                elif not chip_state["chip_2"]:
+                    chip_state["chip_2"] = name
+                    chip_2.style("visibility:visible")
+            else:
+                ui.label()
+
+        def delete_chip(chip):
+            if chip == chip_1:
+                chip_state["chip_1"] = None
+            else:
+                chip_state["chip_2"] = None
+
+            chip.style("visibility:hidden")
+
         with ui.element("div").classes(
             "w-full flex flex-row justify-center content-center gap-x-4 gap-y-6 pt-6"
         ):
-            with ui.element("div").classes(
-                "w-[210px] h-[40px] bg-blue-500 rounded-lg outline outline-offset-4 "
-                "outline-white"
-            ):
-                with ui.row().classes(
-                    "w-full h-full flex flex-row justify-between content-center "
-                    "items-center"
-                ):
-                    ui.label("WHU Tang Clan").classes(
-                        "text-white pl-2 line-clamp-1 max-w-[150px]"
-                    )
-                    ui.icon("cancel", size="25px").classes(
-                        "cursor-pointer pr-2 text-slate-50 hover:text-slate-400"
-                    )
+            chip_1, manager_name_1, delete_chip_1 = manager_chip("WHU Tang Clan", True)
+            chip_2, manager_name_2, delete_chip_2 = manager_chip(
+                "Ruislip Rejects", False
+            )
 
-            with ui.element("div").classes(
-                "w-[210px] h-[40px] bg-red-500 rounded-lg outline outline-offset-4 "
-                "outline-white"
-            ):
-                with ui.row().classes(
-                    "w-full h-full flex flex-row justify-between content-center "
-                    "items-center"
-                ):
-                    ui.label("Ruislip Rejects").classes(
-                        "text-white pl-2 line-clamp-1 max-w-[150px]"
-                    )
-                    ui.icon("cancel", size="25px").classes(
-                        "cursor-pointer pr-2 text-slate-50 hover:text-slate-400"
-                    )
+            manager_name_1.bind_text_from(chip_state, "chip_1")
+            manager_name_2.bind_text_from(chip_state, "chip_2")
+
+        delete_chip_1.on("click", lambda x: delete_chip(chip_1))
+
+        delete_chip_2.on("click", lambda x: delete_chip(chip_2))
+
+        input_1.on("keydown.enter", lambda x: add_chip(input_1.value))
+
+        # chip_1.classes("-z-10")
 
         with ui.element("div").classes("w-full flex flex-row justify-center pt-6"):
-            ui.button("Compare", icon="change_circle").classes(
-                "w-[170px] h-[50px]"
-            ).props('rounded text-color="black" size="md" color="white"')
+            with ui.button().classes("w-[170px] h-[50px]").props(
+                'rounded color="white" '
+            ) as compare_button:
+                ui.label("Compare").classes("text-black text-md")
+                ui.icon("change_circle", size="35px").classes("text-black")
+
+        compare_button.style("visibility:hidden")
 
     #         with ui.tabs().classes("text-blue-4") as tabs:
     #             manager_id_search_tab = ui.tab("Manager ID").classes(
