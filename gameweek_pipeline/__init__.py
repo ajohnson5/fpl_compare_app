c787d59ff97ba09ccecc8b3d34a72dbb1b5501a8
Alfie Johnson <ag.johnson1@hotmail.com>
2023-07-27 19:45:53 +0100

    Add asset job so slack sensors can function properly

--- gameweek_pipeline/__init__.py
+++ gameweek_pipeline/__init__.py
@@ -1,9 +1,19 @@
-from dagster import Definitions
+from dagster import Definitions, define_asset_job
 from gameweek_pipeline.assets import players
-from gameweek_pipeline.sensors import gameweek_sensor
+from gameweek_pipeline.sensors import (
+    make_gameweek_sensor,
+    my_slack_on_run_failure,
+    my_slack_on_run_success,
+)
+
+
+fpl_asset_job = define_asset_job(name="fpl_asset_job")
+
+gameweek_sensor = make_gameweek_sensor(fpl_asset_job)
 
 
 defos = Definitions(
     assets=[players],
-    sensors=[gameweek_sensor],
+    jobs=[fpl_asset_job],
+    sensors=[gameweek_sensor, my_slack_on_run_failure, my_slack_on_run_success],
 )
