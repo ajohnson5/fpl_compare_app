3973e3db7c384c830e00c4d04d382de142adaf72
Alfie Johnson <ag.johnson1@hotmail.com>
2023-07-27 19:44:13 +0100

    Update sensor to maker function - Accept job argument so sensor kicks off job rather than asset

--- gameweek_pipeline/sensors/gameweek_sensor.py
+++ gameweek_pipeline/sensors/gameweek_sensor.py
@@ -17,35 +17,40 @@ def check_gameweek(gw):
     return False
 
 
-@sensor(
-    asset_selection=AssetSelection.all(),
-    minimum_interval_seconds=3600,
-    default_status=DefaultSensorStatus.RUNNING,
-)
-def gameweek_sensor(context):
-    """
-    Sensor checks to see if gameweek has been completed and adds dynamic partition for
-    that gameweek and then materializes all assets with that partition key.
-
-    Args:
-        context (OpExecutionContext): object provides system information
-        such as resources, config and partitions
-
-    Returns:
-        SkipRequest: If gameweek is not finished a SkipRequest is returned so assets
-        are not materialized
-        RunRequest: If gameweek is completed then assets are materialized for newly
-        added partition
-    """
-
-    partition_key = context.cursor or "1"
-    if not check_gameweek(partition_key):
-        return SkipReason("Current gameweek is not completed")
-
-    context.instance.add_dynamic_partitions("gameweek_partitions_def", [partition_key])
-    run_request = RunRequest(
-        run_key=partition_key, run_config={}, partition_key=partition_key
+def make_gameweek_sensor(job):
+    @sensor(
+        job=job,
+        minimum_interval_seconds=3600,
+        default_status=DefaultSensorStatus.RUNNING,
     )
-    # Increment cursor by one
-    context.update_cursor(str(int(partition_key) + 1))
-    return run_request
+    def gameweek_sensor(context):
+        """
+        Sensor checks to see if gameweek has been completed and adds dynamic partition
+        for that gameweek and then materializes all assets with that partition key.
+
+        Args:
+            context (OpExecutionContext): object provides system information
+            such as resources, config and partitions
+
+        Returns:
+            SkipRequest: If gameweek is not finished a SkipRequest is returned so assets
+            are not materialized
+            RunRequest: If gameweek is completed then assets are materialized for newly
+            added partition
+        """
+
+        partition_key = context.cursor or "1"
+        if not check_gameweek(partition_key):
+            return SkipReason("Current gameweek is not completed")
+
+        context.instance.add_dynamic_partitions(
+            "gameweek_partitions_def", [partition_key]
+        )
+        run_request = RunRequest(
+            run_key=partition_key, run_config={}, partition_key=partition_key
+        )
+        # Increment cursor by one
+        context.update_cursor(str(int(partition_key) + 1))
+        return run_request
+
+    return gameweek_sensor
