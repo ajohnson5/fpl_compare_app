06e38e0df77fa79d7f31b586017f6be12f9fa608
Alfie Johnson <ag.johnson1@hotmail.com>
2023-08-28 18:56:51 +0100

    Add team_name_short field to players collection

--- gameweek_pipeline/assets/players.py
+++ gameweek_pipeline/assets/players.py
@@ -25,6 +25,29 @@ teams = {
     20: "Wolves",
 }
 
+teams_shorted = {
+    1: "ARS",
+    2: "AVL",
+    3: "BOU",
+    4: "BRE",
+    5: "BHA",
+    6: "BUR",
+    7: "CHE",
+    8: "CRY",
+    9: "EVE",
+    10: "FUL",
+    11: "LIV",
+    12: "LUT",
+    13: "MCI",
+    14: "MUN",
+    15: "NEW",
+    16: "NFO",
+    17: "SHU",
+    18: "TOT",
+    19: "WHU",
+    20: "WOL",
+}
+
 #####################################################################
 #####################################################################
 ############################## Asset ################################
@@ -78,6 +101,7 @@ def get_players(gw):
             "second_name": player["second_name"],
             "web_name": player["web_name"],
             "team_name": teams[player["team"]],
+            "team_name_short": teams_shorted[player["team"]],
             "total_points": player["total_points"],
             "actual_position": player["element_type"],
         }
