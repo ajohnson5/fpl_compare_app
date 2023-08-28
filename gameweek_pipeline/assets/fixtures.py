63001a914dd747f84e44205c6118d002c34b7945
Alfie Johnson <ag.johnson1@hotmail.com>
2023-08-28 15:48:54 +0100

    Add fixtures asset which adds 5 weeks of gameweek data for next 5 fixtures for each team

---
+++ gameweek_pipeline/assets/fixtures.py
@@ -0,0 +1,92 @@
+from dagster import asset
+import requests
+from gameweek_pipeline.partitions import gameweek_partitions_def
+
+teams = {
+    1: "Arsenal",
+    2: "Aston Villa",
+    3: "Bournemouth",
+    4: "Brentford",
+    5: "Brighton",
+    6: "Burnley",
+    7: "Chelsea",
+    8: "Crystal Palace",
+    9: "Everton",
+    10: "Fulham",
+    11: "Liverpool",
+    12: "Luton",
+    13: "Man City",
+    14: "Man Utd",
+    15: "Newcastle",
+    16: "Nott'm Forest",
+    17: "Sheffield Utd",
+    18: "Spurs",
+    19: "West Ham",
+    20: "Wolves",
+}
+
+teams_shorted = {
+    1: "ARS",
+    2: "AVL",
+    3: "BOU",
+    4: "BRE",
+    5: "BHA",
+    6: "BUR",
+    7: "CHE",
+    8: "CRY",
+    9: "EVE",
+    10: "FUL",
+    11: "LIV",
+    12: "LUT",
+    13: "MCI",
+    14: "MUN",
+    15: "NEW",
+    16: "NFO",
+    17: "SHU",
+    18: "TOT",
+    19: "WHU",
+    20: "WOL",
+}
+
+#####################################################################
+#####################################################################
+############################## Asset ################################
+#####################################################################
+#####################################################################
+
+
+@asset(
+    partitions_def=gameweek_partitions_def, required_resource_keys={"firestore_client"}
+)
+def fixtures(context) -> None:
+    upcoming_fixtures = get_next_fixtures(context.partition_key)
+
+    context.resources.firestore_client.load_batch("fixtures", upcoming_fixtures)
+
+    return None
+
+
+#####################################################################
+#####################################################################
+############################ Functions ##############################
+#####################################################################
+#####################################################################
+
+
+def get_next_fixtures(gw):
+    fixtures = {i: [] for i in range(1, 21)}
+
+    for j in range(1, 11):
+        url = f"https://fantasy.premierleague.com/api/fixtures/?event={int(gw)+j}"
+        req = requests.get(url).json()
+
+        for fixture in req:
+            fixtures[fixture["team_h"]].append(teams_shorted[fixture["team_a"]])
+            fixtures[fixture["team_a"]].append(teams_shorted[fixture["team_h"]])
+
+    next_5_fixtures = {f"gameweek_{int(gw)+k}": {} for k in range(5)}
+    for k in range(5):
+        for key, value in fixtures.items():
+            next_5_fixtures[f"gameweek_{int(gw)+k}"][teams[key]] = value[k : k + 5]
+
+    return next_5_fixtures
