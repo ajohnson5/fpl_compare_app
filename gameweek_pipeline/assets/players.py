c9471b46b2e4558f8fee31dd8307887c2e81576c
Alfie Johnson <ag.johnson1@hotmail.com>
2024-01-01 15:43:41 +0000

    Move duplicated variables to fixtures.py

--- gameweek_pipeline/assets/players.py
+++ gameweek_pipeline/assets/players.py
@@ -1,79 +1,28 @@
 from dagster import asset
 import requests
 from gameweek_pipeline.partitions import gameweek_partitions_def
-
-teams = {
-    1: "Arsenal",
-    2: "Aston Villa",
-    3: "Bournemouth",
-    4: "Brentford",
-    5: "Brighton",
-    6: "Burnley",
-    7: "Chelsea",
-    8: "Crystal Palace",
-    9: "Everton",
-    10: "Fulham",
-    11: "Liverpool",
-    12: "Luton",
-    13: "Man City",
-    14: "Man Utd",
-    15: "Newcastle",
-    16: "Nott'm Forest",
-    17: "Sheffield Utd",
-    18: "Spurs",
-    19: "West Ham",
-    20: "Wolves",
-}
-
-teams_shorted = {
-    1: "ARS",
-    2: "AVL",
-    3: "BOU",
-    4: "BRE",
-    5: "BHA",
-    6: "BUR",
-    7: "CHE",
-    8: "CRY",
-    9: "EVE",
-    10: "FUL",
-    11: "LIV",
-    12: "LUT",
-    13: "MCI",
-    14: "MUN",
-    15: "NEW",
-    16: "NFO",
-    17: "SHU",
-    18: "TOT",
-    19: "WHU",
-    20: "WOL",
-}
-
-#####################################################################
-#####################################################################
-############################## Asset ################################
-#####################################################################
-#####################################################################
+from .fixtures import teams, teams_shorted
 
 
-@asset(
-    partitions_def=gameweek_partitions_def, required_resource_keys={"firestore_client"}
-)
-def players(context) -> None:
-    players = get_gameweeks(context.partition_key)
+def get_players(gw: str) -> dict:
+    url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+    req = requests.get(url).json()
 
-    context.resources.firestore_client.load_batch("players", players)
+    players = {}
+    for player in req["elements"]:
+        players[str(player["id"])] = {
+            "first_name": player["first_name"],
+            "second_name": player["second_name"],
+            "web_name": player["web_name"],
+            "team_name": teams[player["team"]],
+            "team_name_short": teams_shorted[player["team"]],
+            "total_points": player["total_points"],
+            "actual_position": player["element_type"],
+        }
+    return players
 
-    return None
 
-
-#####################################################################
-#####################################################################
-############################ Functions ##############################
-#####################################################################
-#####################################################################
-
-
-def get_gameweeks(gw):
+def get_gameweeks(gw: str) -> dict:
     url = f"https://fantasy.premierleague.com/api/event/{gw}/live/"
     req = requests.get(url).json()
 
@@ -90,19 +39,12 @@ def get_gameweeks(gw):
     return players
 
 
-def get_players(gw):
-    url = "https://fantasy.premierleague.com/api/bootstrap-static/"
-    req = requests.get(url).json()
+@asset(
+    partitions_def=gameweek_partitions_def, required_resource_keys={"firestore_client"}
+)
+def players(context) -> None:
+    players = get_gameweeks(context.partition_key)
 
-    players = {}
-    for player in req["elements"]:
-        players[str(player["id"])] = {
-            "first_name": player["first_name"],
-            "second_name": player["second_name"],
-            "web_name": player["web_name"],
-            "team_name": teams[player["team"]],
-            "team_name_short": teams_shorted[player["team"]],
-            "total_points": player["total_points"],
-            "actual_position": player["element_type"],
-        }
-    return players
+    context.resources.firestore_client.load_batch("players", players)
+
+    return None
