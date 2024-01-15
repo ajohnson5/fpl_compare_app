2f6e819c4304df8b5c670a150a42695287502c0f
Alfie Johnson <ag.johnson1@hotmail.com>
2024-01-15 16:23:05 +0000

    Add error handling for API endpoints

--- gameweek_pipeline/assets/players.py
+++ gameweek_pipeline/assets/players.py
@@ -9,16 +9,21 @@ def get_players(gw: str) -> dict:
     req = requests.get(url).json()
 
     players = {}
-    for player in req["elements"]:
-        players[str(player["id"])] = {
-            "first_name": player["first_name"],
-            "second_name": player["second_name"],
-            "web_name": player["web_name"],
-            "team_name": teams[player["team"]],
-            "team_name_short": teams_shorted[player["team"]],
-            "total_points": player["total_points"],
-            "actual_position": player["element_type"],
-        }
+
+    try:
+        for player in req["elements"]:
+            players[str(player["id"])] = {
+                "first_name": player["first_name"],
+                "second_name": player["second_name"],
+                "web_name": player["web_name"],
+                "team_name": teams[player["team"]],
+                "team_name_short": teams_shorted[player["team"]],
+                "total_points": player["total_points"],
+                "actual_position": player["element_type"],
+            }
+    except KeyError:
+        raise ("elements key does not exist in the returned JSON. Site down.")
+
     return players
 
 
@@ -28,14 +33,21 @@ def get_gameweeks(gw: str) -> dict:
 
     players = get_players(gw)
 
-    for player in req["elements"]:
-        players[str(player["id"])]["gameweeks"] = {
-            f"gameweek_{gw}": {
-                "minutes": player["stats"]["minutes"],
-                "points": player["stats"]["total_points"],
-                "bonus": player["stats"]["bonus"],
+    try:
+        for player in req["elements"]:
+            players[str(player["id"])]["gameweeks"] = {
+                f"gameweek_{gw}": {
+                    "minutes": player["stats"]["minutes"],
+                    "points": player["stats"]["total_points"],
+                    "bonus": player["stats"]["bonus"],
+                }
             }
-        }
+    except KeyError:
+        raise (
+            f"elements key does not exist in the returned JSON. Gameweek {gw} "
+            "exceeds bounds."
+        )
+
     return players
 
 
